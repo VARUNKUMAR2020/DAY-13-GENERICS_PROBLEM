e0eeb880ba705301e224905b8c3f25f8eb6f1608
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-20 12:06:33 +0530

    ADD:GREATEST NUMBER AMONG THREE

--- Generics.java
+++ Generics.java
@@ -1,8 +1,20 @@
 package Generics;
-public class Generics 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "GENERIC METHOD BASED PROBLEM" );
-    }
+
+public class Generics {
+
+	public <T extends Comparable<T>> T maximum(T x, T y, T z) 
+	{
+		T max = x;
+		if (y.compareTo(max) > 0)
+			max = y;
+		if (z.compareTo(max) > 0)
+			max = z;
+		return max;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("GENERIC METHOD BASED PROBLEM");
+		Generics g = new Generics();
+		System.out.println("GREATEST NUMBER IS : " +g.maximum(10,78,45));
+	}
 }
